0790b3f788e595eee8b379bf256b7fb4815974af
Yusuf <yusuf.isaacs@rapiddeploy.com>
2022-01-15 09:03:58 +0200

    Setup Retrofit

---
+++ app/src/main/java/com/mobile/weatherappglobalkenetic/api/ApiInterface.kt
@@ -0,0 +1,22 @@
+package com.mobile.weatherappglobalkenetic.api
+
+import com.mobile.weatherappglobalkenetic.model.CurrentWeatherInfo
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiInterface {
+
+    @GET("weather")
+    fun getCurrentWeatherInfo(
+        @Query("q") cityName: String?,
+        @Query("units") units: String = "metric"
+    ): Call<CurrentWeatherInfo>
+
+//    @GET("forecast")
+//    fun getForecast(
+//        @Query("q") cityName: String?,
+//        @Query("units") units: String = "metric"
+//    ): Call<FiveDayForecast>
+
+}
